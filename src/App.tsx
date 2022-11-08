f6227a0fa5d35731c1b3f337afb9958318528d3d
yyz <yuanyuzhangyyzz@163.com>
2022-11-08 08:53:02 +0800

    add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { Provider } from 'react-redux';
 import './App.scss';
 import { ClientRoutes } from './ClientRoutes';
-import { SadCrudHeader } from './components/SadCrudHeader/SadCrudHeader.tsx';
+import { SadCrudHeader } from './components/SadCrudHeader';
 import store from './store';
 
-function App() {
+function App(): JSX.Element {
   return (
     <Provider store={store}>
       <div className="App">
